45df71107bffce05ecadc9f98431137b69ab6e79
ruru112233 <esxwaz1234@gmail.com>
2021-06-06 17:59:19 +0900

    ターゲットのスピード調整

--- Assets/Scripts/TargetManager2.cs
+++ Assets/Scripts/TargetManager2.cs
@@ -6,9 +6,9 @@ public class TargetManager2 : MonoBehaviour
 {
     void Update()
     {
-        transform.eulerAngles += new Vector3(0, 0, 90 * Time.deltaTime);
+        transform.eulerAngles += new Vector3(0, 0, 150 * Time.deltaTime);
 
-        transform.localScale -= new Vector3(0.4f * Time.deltaTime, 0.4f * Time.deltaTime, 1);
+        transform.localScale -= new Vector3(1.5f * Time.deltaTime, 1.5f * Time.deltaTime, 1);
         if (transform.localScale.x <= 0.5f)
         {
             transform.localScale = new Vector3(0.8f, 0.8f, 1);
