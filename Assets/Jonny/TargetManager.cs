bd598476469224e362feea8ace2beb99973b2ff7
Jonny <53932961+Project-Jonny@users.noreply.github.com>
2021-06-06 14:30:05 +0900

    エフェクト微調整

--- Assets/Jonny/TargetManager.cs
+++ Assets/Jonny/TargetManager.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class TargetManager : MonoBehaviour
 {
+    [SerializeField] float speed;
+
     void Update()
     {
-        transform.eulerAngles += new Vector3(0, 0, 5);
+        transform.eulerAngles += new Vector3(0, 0, 90 * Time.deltaTime * speed);
 
-        transform.localScale -= new Vector3(0.02f, 0.02f, 1);
+        transform.localScale -= new Vector3(0.4f * Time.deltaTime * speed, 0.4f * Time.deltaTime * speed, 1);
         if (transform.localScale.x <= 0.5f)
         {
             transform.localScale = new Vector3(0.8f, 0.8f, 1);
