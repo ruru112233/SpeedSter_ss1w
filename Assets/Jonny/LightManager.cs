6e22aa83e1f3fce325aec55a8d3fcea1d1e402b2
Jonny <53932961+Project-Jonny@users.noreply.github.com>
2021-06-08 21:26:46 +0900

    最初のタイミングのノーツを作成

--- Assets/Jonny/LightManager.cs
+++ Assets/Jonny/LightManager.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class LightManager : MonoBehaviour
 {
-    [SerializeField] float speed;
-
     [SerializeField] GameObject[] lights;
 
     void Start()
@@ -14,35 +12,6 @@ public class LightManager : MonoBehaviour
         StartCoroutine(Flash());
         StartCoroutine(Flash());
         StartCoroutine(Flash());
-    }
-
-    void Update()
-    {
-        transform.eulerAngles += new Vector3(0, 0, 1 * Time.deltaTime * speed);
-
-        /*if (transform.rotation.z >= 15)
-        {
-            rad = -5;
-        }
-        else if (transform.rotation.z <= -15)
-        {
-            rad = 5;
-        }*/
-    }
-
-    IEnumerator ColorChange()
-    {
-        yield return new WaitForSeconds(2);
-        
-        while(true)
-        {
-            byte red = (byte)Random.Range(1, 256);
-            byte blue = (byte)Random.Range(1, 256);
-            byte green = (byte)Random.Range(1, 256);
-
-            GetComponentInChildren<Light>().color = new Color(red, green, blue);
-            yield return new WaitForSeconds(2);
-        }
     }
 
     IEnumerator Flash()
