ad0ddc76fdb5c6bb09284ba1c15d979ba1dcee38
Jonny <53932961+Project-Jonny@users.noreply.github.com>
2021-06-10 00:09:41 +0900

    ステージの移植

--- Assets/Scripts/TargetManager2.cs
+++ Assets/Scripts/TargetManager2.cs
@@ -8,8 +8,8 @@ public class TargetManager2 : MonoBehaviour
     {
         transform.eulerAngles += new Vector3(0, 0, 120 * Time.deltaTime);
 
-        transform.localScale -= new Vector3(0.5f * Time.deltaTime, 0.5f * Time.deltaTime, 1);
-        if (transform.localScale.x <= 2.5f)
+        transform.localScale -= new Vector3(2.5f * Time.deltaTime, 2.5f * Time.deltaTime, 1);
+        if (transform.localScale.x <= 2.0f)
         {
             transform.localScale = new Vector3(3, 3, 1);
         }
